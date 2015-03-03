f890bf420318513545c735c754cc17a4a3ed6f01
meghart <meghan.hartman@nyu.edu>
2015-03-03 18:03:25 -0500

    secrets.yml

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,7 +3,7 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image,
                   :storage => :s3,
-                  :s3_credentials => "#{Rails.root}/config/s3.yml",
+                  :s3_credentials => "#{Rails.root}/config/secrets.yml",
                   :path => "/:style/:id/:filename"
 
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
